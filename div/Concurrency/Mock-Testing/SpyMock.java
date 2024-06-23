500f1481136b7ea8ff2cf4429e90836c91132a98
durresi2 <durresi2@students.zhaw.ch>
2024-06-23 10:45:21 +0200

    added spy mock

---
+++ div/Concurrency/Mock-Testing/SpyMock.java
@@ -0,0 +1,29 @@
+package Concurrency.Mock.*
+
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+
+public class SpyMock {
+    public static void main(String[] args) {
+        // Erstellen eines echten Objekts
+        MyClass myObject = new MyClass();
+
+        // Spionieren des echten Objekts
+        MyClass spiedObject = spy(myObject);
+
+        // Definieren des Verhaltens für eine Methode des echten Objekts
+        doReturn("Mocked Value").when(spiedObject).getValue();
+
+        // Aufruf der echten Methode
+        System.out.println("Real Value: " + myObject.getValue());
+
+        // Aufruf der gesperrten Methode
+        System.out.println("Mocked Value: " + spiedObject.getValue());
+    }
+}
+
+class MyClass {
+    public String getValue() {
+        return "Real Value";
+    }
+}
